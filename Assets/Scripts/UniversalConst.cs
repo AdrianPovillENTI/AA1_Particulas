91e93b03160034f6a999fcb52d414d607081d6d6
Adrian Povill <adrian.povill@estudiant.enti.cat>
2026-03-25 19:44:40 +0100

    Delete old project

--- Assets/Scripts/UniversalConst.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class UniversalConst
-{
-    public const float gravitationalConstant = 0.01f;
-    public const float physicsTimeStep = 0.3f;
-}
